bbef84e392af2aa7d3c1291c9a31c07e265198fc
gatosaurio <hermeskybalion@hotmail.com>
2015-06-25 22:38:11 -0300

    social share tweaks

--- client/main.js
+++ client/main.js
@@ -7,16 +7,15 @@ Meteor.startup(function() {
 });
 
 ShareIt.configure({
-  sites: {                // nested object for extra configurations
-        'facebook': {
-            'appId': '677863165649310'   // use sharer.php when it's null, otherwise use share dialog
-        }
-    },
-  useFB: true,          // boolean (default: true)
-  useTwitter: true,     // boolean (default: true)
-  useGoogle: false,      // boolean (default: true)
-  usePinterest: false,
-  classes: "btn", // string (default: 'large btn')
-  iconOnly: false,       // boolean (default: false)
-  applyColors: true     // boolean (default: true)
+  useFB: true,
+  useTwitter: true,
+  useGoogle: true,
+  classes: "btn",
+  iconOnly: false,
+  applyColors: true,
+  sites: {
+    'facebook': {
+        'appId': ''
+    }
+    }
 });
--- client/templates/professor/social-share.js
+++ client/templates/professor/social-share.js
@@ -1,13 +1,15 @@
 Template.socialShare.rendered = function(){
-	$('.fb-share').text("Compartir en Facebook");
-	$('.tw-share').text("Compartir en Twitter");
+	$('.fb-share').text("Facebook");
+	$('.tw-share').text("Twitter");
+	$('.google-share').text("Google+");
 };
 
 
 Template.socialShare.helpers({
 	shareData: function() {
     var resp = {
-			title: "Mira el rating del profesor " +this.name+ " en"
+			title: "Mira el rating del profesor " +this.name+ " en",
+			url: "http://telepathiclove.com/professor/"+this._id
     };
     return resp;
   }
