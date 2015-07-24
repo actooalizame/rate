051eafbc474c4af3031a814067dcd066b32e7c6e
gatosaurio <hermeskybalion@hotmail.com>
2015-07-24 04:14:07 -0300

    added schools from csv

--- client/main.js
+++ client/main.js
@@ -19,6 +19,11 @@ Meteor.startup(function() {
   Session.setDefault('countTags', 0);
 });
 
+UI.registerHelper('formatTime', function(context, options) {
+  if(context)
+    return moment(context).format('DD/MM/YYYY, hh:mm');
+});
+
 ShareIt.configure({
   useFB: true,
   useTwitter: true,
